eb0307399b9798103278853c2f52eddd9823bc86
Wangzhiyuan <344182022@qq.com>
2025-04-13 12:24:02 +0700

    Update hw02_01.cpp

--- hw02_01.cpp
+++ hw02_01.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int r, h;
+	cout << "r and h:";
+	cin >> r >> h;
+
+	float pi = 3.14159265;
+	float S = pi * r * r * 2 + 2 * pi * r * h;
+	float V = pi * r * r * h;
+
+	cout << "S = " << S << endl;
+	cout << "V =" << V << endl;
+
+	return 0;
+}
